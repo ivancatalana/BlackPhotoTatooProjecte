73331d0019d8129b5662f8aa4876ae3e3966d54f
ivancatalana <ivancatalana@gmail.com>
2022-11-30 22:10:09 +0100

    Version con Splashscreen

---
+++ app/src/main/java/com/example/blackphototatoo/FullscreenStartFragment.java
@@ -0,0 +1,49 @@
+package com.example.blackphototatoo;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class FullscreenStartFragment extends Fragment {
+
+    NavController navController;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fullscreen_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Do something after 3000ms
+                navController.navigate(R.id.action_fullscreenStartFragment_to_loginFragment);
+
+            }
+        }, 3000);
+
+    }
+
+
+}
